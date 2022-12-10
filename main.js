1e801ec5542d1524a58e313bc08f64ebf69b002e
Desoroxxx <desoroxxx@gmail.com>
2022-12-11 00:36:06 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,80 @@
+// First, create a new Worker instance and pass it the URL of your JavaScript file
+const worker = new Worker('background.js');
+
+// Next, send data to the worker by calling the `postMessage` method on the Worker instance
+worker.postMessage({ someData: 'hello there!' });
+
+// Finally, listen for messages from the worker by setting up an event listener for the `message` event
+worker.addEventListener('message', event => {
+  console.log(`Received message from worker:`, event.data);
+});
+
+////////////////////////////
+//About me text animation//
+//////////////////////////
+
+const sentences = ["making code and things faster and more efficient and simpler and easier to look at and easier to understand.","optimizing things!"];
+let i = 0;
+let counter;
+
+setTimeout(deleteText, 3000);
+
+function deleteText() {
+    let word = sentences[i].split("");
+    
+    var loopDeleting = function() {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('text').innerHTML = word.join("");
+        } else {
+            i++;
+            writeText();
+        }
+        counter = setTimeout(loopDeleting, 50);
+    }            
+    loopDeleting();
+}
+
+function writeText() {
+    let word = sentences[i].split("");
+
+    var loopWrite = function() {
+        if (word.length > 0) {                    
+            document.getElementById('text').innerHTML += word.shift();
+        } else {
+            setTimeout(removeProperty, 1000);
+        }
+        counter = setTimeout(loopWrite, 200);
+   }
+   loopWrite();
+}
+
+// Remove the cursor
+function removeProperty() {            
+    document.getElementById('text').style.removeProperty('border-right');
+}
+
+/////////////////////////////////////////
+//Github icon changing with user theme//
+///////////////////////////////////////
+
+// Get all elements with the "icon" class
+const icons = document.querySelectorAll(".icon");
+
+function updateIcons() {
+  // Check if the user's preferred color scheme is light or dark
+  const isLightTheme = window.matchMedia(`(prefers-color-scheme: light)`).matches;
+
+  // Set the src attribute of each icon to the appropriate URL
+  icons.forEach(function(icon) {
+    icon.src = isLightTheme ? 'github-mark.svg' : 'github-mark-white.svg';
+  });
+}
+
+// Listen for changes in the user's preferred color scheme
+window.matchMedia(`(prefers-color-scheme: light)`).addListener(function(e) {
+  updateIcons();
+});
+
+// Update the icons when the page first loads
+updateIcons();
